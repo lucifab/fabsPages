70b82fdaba4ea784b2e32dbdac0e176ab72831a7
lucifab <fabiane.olenka@gmail.com>
2023-08-02 15:19:29 -0700

    Initial changes to adapt for API / large datasets

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-article',
@@ -6,9 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./article.component.scss']
 })
 export class ArticleComponent {
-  title: string;
-  articleDate = new Date();
-  //content : string;
+  @Input('articleTitle') title: string;
+  @Input('articleDate') articleDate = new Date();
+  @Input('articleContents') content : string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pellentesque, ligula ac pellentesque dictum, turpis eros varius quam, non viverra eros justo a ante. Nam elit erat, varius sed ligula laoreet, molestie consequat tellus. Morbi malesuada facilisis neque, condimentum vestibulum dui. Donec vel diam mi. Nunc luctus odio sapien, eu dapibus justo suscipit nec. Duis porttitor urna in posuere pharetra. Ut massa justo, efficitur sodales ex vitae, efficitur malesuada felis. Ut posuere sem vitae augue fringilla dapibus at non augue. Vivamus lobortis, massa sed fermentum ullamcorper, tellus ex mattis ipsum, a venenatis lectus ligula non augue. Donec lobortis massa a nulla finibus fringilla. Nulla egestas orci sed massa placerat, sit amet aliquam ipsum auctor.";
 
   constructor(){
     this.title = 'Sometimes, it is the way it is';
--- src/app/components/main-body/main-body.component.ts
+++ src/app/components/main-body/main-body.component.ts
@@ -1,16 +1,24 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-main-body',
   templateUrl: './main-body.component.html',
   styleUrls: ['./main-body.component.scss']
 })
-export class MainBodyComponent {
+export class MainBodyComponent implements OnChanges {
 
   @Input() selectedContent: string;
 
   constructor(){
     this.selectedContent = 'blog';
+  }
+
+  ngOnChanges(changes: SimpleChanges): void { // Use ngOnChanges to monitor updates of @Input
+    if(changes['selectedContent']){
+      console.log("PREVIOUS:" + changes['selectedContent'].previousValue);
+      console.log("CURRENT:" + changes['selectedContent'].currentValue);
+      switch
+    }
   }
 
 }
