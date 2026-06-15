5f3cb57a1fb700ba2f70c0d018e7ccae88b05b08
lucifab <fabiane.olenka@gmail.com>
2026-06-15 16:54:30 -0700

    Add environments to facilitate testing

--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Post } from '../models/post.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebPostsAPIService {
 
-  private apiUrl='http://localhost:5087'
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://2wbe1ip611.execute-api.us-east-2.amazonaws.com/fabApi'
+};
