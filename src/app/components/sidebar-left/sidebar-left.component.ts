f61b449912cca885c2ac936f2fb6db5d8f3bf53b
lucifab <fabiane.olenka@gmail.com>
2026-06-17 00:16:27 -0700

    style updates so main icon displays user picture if logged in, change button label

--- src/app/components/sidebar-left/sidebar-left.component.ts
+++ src/app/components/sidebar-left/sidebar-left.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MoveDirection, OutMode, Engine } from "tsparticles-engine";
 import { loadFull } from "tsparticles";
 import { faLinkedin, faGithub, faTumblr } from '@fortawesome/free-brands-svg-icons';
+import { CognitoService } from 'src/app/services/cognito-service.service';
 
 
 @Component({
@@ -11,67 +12,69 @@ import { faLinkedin, faGithub, faTumblr } from '@fortawesome/free-brands-svg-ico
     standalone: false
 })
 export class SidebarLeftComponent {
+    
+    cognitoService = inject(CognitoService);
 
-  // Icons
-  faLinkedin = faLinkedin;
-  faGithub = faGithub;
-  faTumblr = faTumblr;
+    // Icons
+    faLinkedin = faLinkedin;
+    faGithub = faGithub;
+    faTumblr = faTumblr;
 
-  //Particle Effects
-  particlesOptions = {
-   
-    fpsLimit: 120,
-    particles: {
-        color: {
-            value: "#ffffff",
-        },
-        links: {
-            color: "#ffffff",
-            distance: 150,
-            enable: false,
-            opacity: 0.5,
-            width: 1,
-        },
-        collisions: {
-            enable: false,
-        },
-        move: {
-            direction: MoveDirection.none,
-            enable: true,
-            outModes: {
-                default: OutMode.bounce,
+    //Particle Effects
+    particlesOptions = {
+
+        fpsLimit: 120,
+        particles: {
+            color: {
+                value: "#ffffff",
             },
-            random: false,
-            speed: 1,
-            straight: false,
-        },
-        number: {
-            density: {
+            links: {
+                color: "#ffffff",
+                distance: 150,
+                enable: false,
+                opacity: 0.5,
+                width: 1,
+            },
+            collisions: {
+                enable: false,
+            },
+            move: {
+                direction: MoveDirection.none,
                 enable: true,
-                area: 800,
+                outModes: {
+                    default: OutMode.bounce,
+                },
+                random: false,
+                speed: 1,
+                straight: false,
             },
-            value: 80,
+            number: {
+                density: {
+                    enable: true,
+                    area: 800,
+                },
+                value: 80,
+            },
+            opacity: {
+                value: 0.4,
+            },
+            shape: {
+                type: "circle",
+            },
+            size: {
+                value: { min: 1, max: 3 },
+            },
         },
-        opacity: {
-            value: 0.4,
-        },
-        shape: {
-            type: "circle",
-        },
-        size: {
-            value: { min: 1, max: 3 },
-        },
-    },
-    detectRetina: true,
-};
+        detectRetina: true,
+    };
 
 
-async particlesInit(engine: Engine): Promise<void> {
+    async particlesInit(engine: Engine): Promise<void> {
 
-    // Starting from 1.19.0 you can add custom presets or shape here, using the current tsParticles instance (main)
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(engine);
-}
+        // Starting from 1.19.0 you can add custom presets or shape here, using the current tsParticles instance (main)
+        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+        // starting from v2 you can add only the features you need reducing the bundle size
+        await loadFull(engine);
+    }
 
 }
